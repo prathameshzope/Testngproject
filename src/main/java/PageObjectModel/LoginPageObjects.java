387ff993021d256edc6def809ecf8f5ddf1b7086
prathameshzope <somyapatidar55@gmail.com>
2023-02-26 00:16:21 +0530

    added one new comment

--- src/main/java/PageObjectModel/LoginPageObjects.java
+++ src/main/java/PageObjectModel/LoginPageObjects.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 public class LoginPageObjects {
 
 
-
+        // added new line for github demo purpose
 		// This driver dont have scope
     	public WebDriver driver;
 	
